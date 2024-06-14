95fd88949948890f759ba2bcd43aa03420a633d8
Christoph Baier <chrisby@crispy-coding.org>
2024-06-14 10:09:02 +0200

    first version

---
+++ AssertWrapper.go
@@ -0,0 +1,32 @@
+package shared
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func AssertTrue(t *testing.T, condition bool, msg ...string) {
+	assert.True(t, condition, msg)
+}
+
+func AssertEqual(t *testing.T, expected interface{}, actual interface{}, msg ...string) {
+	assert.Equal(t, expected, actual, msg)
+}
+
+func AssertPanics(t *testing.T, f PanicFunc, msg ...string) {
+	assert.Panics(t, assert.PanicTestFunc(f), msg)
+}
+
+func AssertFail(t *testing.T, msg string) {
+	assert.Fail(t, msg)
+}
+
+func AssertNil(t *testing.T, object interface{}, msg ...string) {
+	assert.Nil(t, object, msg)
+}
+
+func AssertNotNil(t *testing.T, object interface{}, msg ...string) {
+	assert.NotNil(t, object, msg)
+}
+
+type PanicFunc func()
---
+++ Logger.go
@@ -0,0 +1,98 @@
+package shared
+
+import (
+	"github.com/rs/zerolog"
+	"os"
+	"time"
+)
+
+type LogLevelValue int
+
+const (
+	TRACE LogLevelValue = iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
+)
+
+var LogLevel LogLevelValue
+var logger Logger
+
+type Logger interface {
+	Trace(message string, v ...any)
+	Debug(message string, v ...any)
+	Info(message string, v ...any)
+	Warn(message string, v ...any)
+	Error(message string, v ...any)
+	Fatal(message string, v ...any)
+}
+
+func (l LogLevelValue) String() string {
+	return [...]string{"TRACE", "DEBUG", "INFO", "WARN", "ERROR"}[l]
+}
+
+func ProvideLogger() Logger {
+	logFile, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic("Failed to open log file")
+	}
+
+	multi := zerolog.MultiLevelWriter(
+		zerolog.ConsoleWriter{
+			Out:        logFile,
+			TimeFormat: time.RFC3339,
+			NoColor:    true,
+		},
+		zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: time.RFC3339,
+		},
+	)
+
+	var zerologLibraryLogLevel zerolog.Level
+	switch LogLevel {
+	case TRACE:
+		zerologLibraryLogLevel = zerolog.TraceLevel
+	case DEBUG:
+		zerologLibraryLogLevel = zerolog.DebugLevel
+	case INFO:
+		zerologLibraryLogLevel = zerolog.InfoLevel
+	case WARN:
+		zerologLibraryLogLevel = zerolog.WarnLevel
+	case ERROR:
+		zerologLibraryLogLevel = zerolog.ErrorLevel
+	default:
+		panic("No valid log level set.")
+	}
+	logger := zerolog.New(multi).Level(zerologLibraryLogLevel).With().Timestamp().CallerWithSkipFrameCount(3).Logger()
+	return &MyLogger{&logger}
+}
+
+type MyLogger struct {
+	Logger *zerolog.Logger
+}
+
+func (m *MyLogger) Trace(format string, v ...any) {
+	m.Logger.Trace().Msgf(format, v...)
+}
+
+func (m *MyLogger) Debug(format string, v ...any) {
+	m.Logger.Debug().Msgf(format, v...)
+}
+
+func (m *MyLogger) Info(format string, v ...any) {
+	m.Logger.Info().Msgf(format, v...)
+}
+
+func (m *MyLogger) Warn(format string, v ...any) {
+	m.Logger.Warn().Msgf(format, v...)
+}
+
+func (m *MyLogger) Error(format string, v ...any) {
+	m.Logger.Error().Msgf(format, v...)
+}
+
+func (m *MyLogger) Fatal(format string, v ...any) {
+	m.Logger.Fatal().Msgf(format, v...)
+}
