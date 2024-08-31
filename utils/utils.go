79ee7f186ae8a2d1b062d7863c8c0a66e7046d1a
Christoph Baier <chrisby@crispy-coding.org>
2024-08-31 15:56:20 +0200

    added CORS handler

--- utils/utils.go
+++ utils/utils.go
@@ -148,3 +148,19 @@ func GetErrMsg(actualStatusCode int, respBodyMsg string) string {
 	}
 	return fmt.Sprintf("Expected status code 200, but got %d.%s", actualStatusCode, msg)
 }
+
+// GetCorsDisablingHandler This is necessary to allow cross-origin requests from the ocelot-cloud GUI to the hub.
+// The "Origin" header is managed and checked with custom logic to prevent CSRF attacks.
+func GetCorsDisablingHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
