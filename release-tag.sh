7fae169c11dcd6b197fcc0305847f4f51486d3ca
Christoph Baier <chrisby@crispy-coding.org>
2024-12-17 11:27:03 +0100

    refactoring

--- release-tag.sh
+++ release-tag.sh
@@ -9,6 +9,6 @@ read -p "Enter the new tag name: " NEW_TAG
 git commit -am "refactoring" || true
 git push
 git tag $NEW_TAG
-git push origin $NEW_TAGy
+git push origin $NEW_TAG
 
 echo "Tag $NEW_TAG created and pushed to origin."
