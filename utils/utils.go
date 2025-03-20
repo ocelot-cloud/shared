7f7cf6c42c56bc5ba6d760f923fb0c753442de1f
Christoph Baier <chrisby@crispy-coding.org>
2025-03-20 15:30:16 +0100

    refactoring

--- utils/utils.go
+++ utils/utils.go
@@ -172,6 +172,10 @@ func GetCorsDisablingHandler(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
