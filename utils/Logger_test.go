b2266af9bfc7406fce0683196b98efb089d6576e
Christoph Baier <chrisby@crispy-coding.org>
2025-07-19 18:11:01 +0200

    refactoring

--- utils/Logger_test.go
+++ utils/Logger_test.go
@@ -16,3 +16,19 @@ func TestLoggingVisually(t *testing.T) {
 	logger.Error("This is an info message", "error", "some-error")             // TODO
 	logger.Error("This is an info message", "error", errors.New("some-error")) // TODO
 }
+
+/* TODO I want smart error logging with structured logging and stack traces, plan:
+type OcError struct {
+	errorMessage string
+	errorStack   string
+	Context 	 map[string]any
+}
+
+* logger.CreateError()
+* high level function logs the error; prints the error message and context in a single line, and below that with pretty formatting is the stack trace
+* func AddContext(string...) -> make sure its two args, the odd index arg must be string, the even index arg can be of any type
+* when some methods are not used correctly, e.g. type errors etc, then an error should be logged as the developer used it wrongly
+* actually very lightweight as the error object is not recreated all the time, but you rather add changes directly to the existing error object
+* condition: all errors must be created through my logging library, so that the stack trace is always available, and operations work; using operations on "normal" errors should work but will cause an error log for the developer
+* maybe I should create a small, reusable library for that
+*/
